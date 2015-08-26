fe39fb53d18cc65ea2f6b70c3d3421cec43d9833
Sebastian Stamm <sebastian.stamm@camunda.com>
2015-08-26 18:10:24 +0200

    :camel: (frontend): load and display story text

--- frontend/main.js
+++ frontend/main.js
@@ -2,25 +2,58 @@ window.addEventListener('load', function(evt) {
   'use strict';
 
   var GAME_ID = '';
+  var currentTask = '';
+
+  var addLine = function(text) {
+    var line = document.createElement('li');
+    line.textContent = text;
+    document.getElementById('story').appendChild(line);
+  };
+
+  var requestVariables = function() {
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+          if(xmlhttp.status == 200){
+            var jsonResponse = JSON.parse(xmlhttp.responseText);
+
+            console.log('done loading variables', jsonResponse);
+
+            addLine(jsonResponse.storyText.value);
+
+          } else {
+            console.log('error loading variABLES', xmlhttp);
+          }
+        }
+    };
+
+    xmlhttp.open('GET', 'http://ec2-52-19-141-24.eu-west-1.compute.amazonaws.com:8080/engine-rest/process-instance/'+GAME_ID+'/variables/?deserializeValues=false', true);
+
+    xmlhttp.send();
+
+  };
 
   var requestFirstTask = function() {
     var xmlhttp = new XMLHttpRequest();
 
-    var taskID = '';
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
           if(xmlhttp.status == 200){
             console.log('done loading character creation', xmlhttp);
             var jsonResponse = JSON.parse(xmlhttp.responseText);
-            taskID = jsonResponse.id;
+            currentTask = jsonResponse.id;
 
+            // get the variables
+
+            requestVariables();
           } else {
             console.log('error loading character creation', xmlhttp);
           }
         }
     };
 
-    xmlhttp.open("GET", "http://ec2-52-19-141-24.eu-west-1.compute.amazonaws.com:8080/engine-rest/task/?processInstanceId="+GAME_ID, true);
+    xmlhttp.open('GET', 'http://ec2-52-19-141-24.eu-west-1.compute.amazonaws.com:8080/engine-rest/task/?processInstanceId='+GAME_ID, true);
 
     xmlhttp.send();
   };
@@ -42,9 +75,9 @@ window.addEventListener('load', function(evt) {
         }
     };
 
-    xmlhttp.open("POST", "http://ec2-52-19-141-24.eu-west-1.compute.amazonaws.com:8080/engine-rest/process-definition/key/CharacterCreator/start", true);
+    xmlhttp.open('POST', 'http://ec2-52-19-141-24.eu-west-1.compute.amazonaws.com:8080/engine-rest/process-definition/key/CharacterCreator/start', true);
 
-    xmlhttp.setRequestHeader("Content-type", "application/json");
+    xmlhttp.setRequestHeader('Content-type', 'application/json');
 
     xmlhttp.send(JSON.stringify({
       variables: {}
