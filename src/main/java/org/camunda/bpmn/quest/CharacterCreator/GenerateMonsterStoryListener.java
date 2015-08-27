219108fdbfa7d11c30e3d5cd7fc4793ed03288fb
Jakob Freund <jakob.freund@camunda.com>
2015-08-27 09:40:33 +0200

    :camel: (monstermodel): refactore Monster to have story inside MonsterModel

--- src/main/java/org/camunda/bpmn/quest/CharacterCreator/GenerateMonsterStoryListener.java
+++ src/main/java/org/camunda/bpmn/quest/CharacterCreator/GenerateMonsterStoryListener.java
@@ -7,42 +7,7 @@ public class GenerateMonsterStoryListener implements TaskListener {
 
 	@Override
 	public void notify(DelegateTask delegateTask) {
-		String monsterStory = (String) delegateTask.getVariable("storyText");
-		
-		CharacterModel monster = (CharacterModel) delegateTask.getVariable("thisMonster");
-		
-		String monsterId = monster.getId();
-		
-		monsterStory = getMonsterStory(monsterId);
-
-		delegateTask.setVariable("storyText", monsterStory);
-		
+		MonsterModel monster = (MonsterModel) delegateTask.getVariable("thisMonster");
+		delegateTask.setVariable("storyText", monster.getMonsterStory());
 	}
-
-	private String getMonsterStory(String monsterId) 
-	{
-		String newMonsterStory = "";
-		 switch (monsterId) {
-		 
-         case "alfredo":  newMonsterStory = "A story about Alfredo \n He's small and no one likes him - but for good reason. He's a Jerk you should "
-         		+ "probably crush him while you have the chance!";	
-                  break;
-         case "booneeda": newMonsterStory = "a story about Booneeda \n She's a fairly mean lady. I once saw her eat a kitten, a LIVE kitten. It was a very"
-         		+ "sad day... AVENGE THE KITTEN!!" ;
-                  break;
-         case "falseoracle":  newMonsterStory = "a story about False Oracle \n Well he's big very big, some would say overweight but wouldn't"
-         		+ "want to affend the guy. He's got a serious tempter and could probably crush you to death with his belly flaps... bad way to go - but without"
-         		+ "risk there is no reward go forth! kill the fat jerk";
-                  break;
-         case "lordwebsfear":  newMonsterStory = "a story about Lord Websfear \n People know that he's made of evil and say that if you say his name"
-         		+ "3 times in a mirror he'll show up and why you're acting so stupid. He really hurts a lot of peoples feelings! MURDER HIM!";
-                  break;
-         
-         default: newMonsterStory = "This monster is mysterious as FUCK!";
-         
-		 }
-        
-		return newMonsterStory;
-	}
-
 }
--- src/main/java/org/camunda/bpmn/quest/CharacterCreator/MaterializeMonsterDelegate.java
+++ src/main/java/org/camunda/bpmn/quest/CharacterCreator/MaterializeMonsterDelegate.java
@@ -14,12 +14,12 @@ public class MaterializeMonsterDelegate implements JavaDelegate {
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
 		
-		List<CharacterModel> monsters = generateMonsterPool();
+		List<MonsterModel> monsters = generateMonsterPool();
 		
 		Random rn = new Random();
 		int randomNumber = rn.nextInt(4);
 		
-		CharacterModel thisMonster = monsters.get(randomNumber);
+		MonsterModel thisMonster = monsters.get(randomNumber);
 		
 		ObjectValue monsterDataValue = Variables.objectValue(thisMonster)
 				  .serializationDataFormat(Variables.SerializationDataFormats.JSON)
@@ -29,11 +29,11 @@ public class MaterializeMonsterDelegate implements JavaDelegate {
 		
 	}
 	
-	private List<CharacterModel> generateMonsterPool () {
+	private List<MonsterModel> generateMonsterPool () {
 		
-		List<CharacterModel> monsters = new ArrayList<CharacterModel>();
+		List<MonsterModel> monsters = new ArrayList<MonsterModel>();
 
-		monsters.add(new CharacterModel(
+		monsters.add(new MonsterModel(
 				"alfredo",
 				"Alfredo",
 				30, // Strength
@@ -43,10 +43,12 @@ public class MaterializeMonsterDelegate implements JavaDelegate {
 				30, // Intelligence
 				30, // Agility
 				30, // Luck
-				10 // Experience Points
+				10, // Experience Points
+				"A story about Alfredo \n He's small and no one likes him - but for good reason. He's a Jerk you should "
+         		+ "probably crush him while you have the chance!" // Monster Story
 				));
 	
-		monsters.add(new CharacterModel(
+		monsters.add(new MonsterModel(
 				"booneeda",
 				"Boo Needa",
 				40, // Strength
@@ -56,10 +58,12 @@ public class MaterializeMonsterDelegate implements JavaDelegate {
 				30, // Intelligence
 				70, // Agility
 				40, // Luck
-				40 // Experience Points
+				40, // Experience Points
+				"a story about Boon Needa \n She's a fairly mean lady. I once saw her eat a kitten, a LIVE kitten. It was a very"
+         		+ "sad day... AVENGE THE KITTEN!!"
 				));
 
-		monsters.add(new CharacterModel(
+		monsters.add(new MonsterModel(
 				"falseoracle",
 				"The False Oracle",
 				70, // Strength
@@ -69,10 +73,13 @@ public class MaterializeMonsterDelegate implements JavaDelegate {
 				30, // Intelligence
 				30, // Agility
 				10, // Luck
-				70 // Experience Points
+				70, // Experience Points
+				"a story about False Oracle \n Well he's big very big, some would say overweight but wouldn't"
+         		+ "want to affend the guy. He's got a serious tempter and could probably crush you to death with his belly flaps... bad way to go - but without"
+         		+ "risk there is no reward go forth! kill the fat jerk"
 				));
 		
-		monsters.add(new CharacterModel(
+		monsters.add(new MonsterModel(
 				"lordwebsfear",
 				"Lord Web's Fear",
 				100, // Strength
@@ -82,7 +89,9 @@ public class MaterializeMonsterDelegate implements JavaDelegate {
 				30, // Intelligence
 				10, // Agility
 				20, // Luck
-				100 // Experience Points
+				100, // Experience Points
+				"a story about Lord Web's Fear \n People know that he's made of evil and say that if you say his name"
+         		+ "3 times in a mirror he'll show up and why you're acting so stupid. He is very hard to install and really hurts a lot of peoples feelings! MURDER HIM!"
 				));
 		
 		return monsters;		
