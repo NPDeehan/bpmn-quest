06463e2cd563b2039be23958b80168f702b4d180
Niall <niall.p.deehan@gmail.com>
2015-08-27 16:14:35 +0200

    Recalculate player life after character creation

---
+++ src/main/java/org/camunda/bpmn/quest/superStoryQuestOne/GenerateVictoryStory.java
@@ -0,0 +1,15 @@
+package org.camunda.bpmn.quest.superStoryQuestOne;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class GenerateVictoryStory implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		
+		
+
+	}
+
+}
