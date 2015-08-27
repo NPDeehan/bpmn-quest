d3cb8f0021db3d33208e5789b71a3e9d4c873f0f
Jakob Freund <jakob.freund@camunda.com>
2015-08-27 15:36:15 +0200

    :smile: (death): brag about it

--- src/main/java/org/camunda/bpmn/quest/death/DyingDelegate.java
+++ src/main/java/org/camunda/bpmn/quest/death/DyingDelegate.java
@@ -14,8 +14,7 @@ public class DyingDelegate implements JavaDelegate {
 
 		StoryModel story = new StoryModel();
 		story.setTitle("You're dead...");
-		story.setDescription("Never mind, you'll live to fight another day... somehow. At least you've gained some experience, so why not tell the world about it!? \n\nWhen you hit 'Continue', you're achievements will be tweeted on the 'Camunda Demo' Account on Twitter, with #bpmnQuest - see you soon for another round of BPMN Quest!" );
-		story.setPicture("");
+		story.setDescription("Never mind, you'll live to fight another day... somehow. At least you've gained some experience, so why not tell the world about it!? \n\nWhen you continue, your achievements will be tweeted on the 'Camunda Demo' Account on Twitter, with #bpmnQuest - see you soon for another round of BPMN Quest!" );
 		story.setPicture("http://ec2-52-19-141-24.eu-west-1.compute.amazonaws.com:8080/CharacterCreator/monsters/img/died.png");
 		
 		story.addOption("Tweet and Finish the Game");
